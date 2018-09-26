ce5660b252367dad8a027566293e2a03767a4793
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-09-26 17:55:54 -0400

    Added a Migration

---
+++ gumi_server/Migrations/20180926215454_AddedQuestionTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Gumi_Server.Migrations
+{
+    public partial class AddedQuestionTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    UpVotes = table.Column<int>(nullable: false),
+                    DownVotes = table.Column<int>(nullable: false),
+                    Postedby = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questions");
+        }
+    }
+}
