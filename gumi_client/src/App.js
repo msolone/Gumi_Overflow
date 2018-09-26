a7cb3185f2c6e4f3047eccf13f62062855fe3273
Michael Solone <michaelsolone@gmail.com>
2018-09-26 16:00:41 -0400

    updated git ignore

--- gumi_client/src/App.js
+++ gumi_client/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h2>GumiOverflow</h2>
+          <input type="text" placeholder="Search..." />
+          <h2>Menu</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
