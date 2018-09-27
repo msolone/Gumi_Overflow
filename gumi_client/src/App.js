cf7f757fe2288197df5a07391ed6e97f35f42023
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-09-27 08:34:15 -0400

    Added Some CSS

--- gumi_client/src/App.js
+++ gumi_client/src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
-          <h2>GumiOverflow</h2>
-          <input type="text" placeholder="Search..." />
+          <div class="search">
+            <h2>GumiOverflow</h2>
+            <input type="text" placeholder="Search..." />
+          </div>
           <h2>Menu</h2>
         </header>
       </div>
