855a7c7b3e06f95457fea3c0dc3c2d8879fa3bc2
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-09-27 07:43:36 -0400

    Deleted Warning

--- gumi_server/gumioverflow_datebaseContext.cs
+++ gumi_server/gumioverflow_datebaseContext.cs
@@ -20,7 +20,6 @@ namespace Gumi_Server
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseNpgsql("server=localhost;database=gumioverflow_database");
             }
         }
